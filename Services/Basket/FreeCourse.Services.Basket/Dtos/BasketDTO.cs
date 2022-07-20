21282e45ca8172ddc450e61d4a6b1c76ecc1f214
Orhun Sözer <orhun.sozer@gmail.com>
2022-07-20 23:36:18 +0300

    Added dtos in BasketAPI.

---
+++ Services/Basket/FreeCourse.Services.Basket/Dtos/BasketDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FreeCourse.Services.Basket.Dtos
+{
+    public class BasketDTO
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public List<BasketItemDTO> Items { get; set; }
+        public decimal TotalPrice
+        {
+            get => Items.Sum(s => s.Price * s.Quantity); 
+        }
+
+    }
+}
