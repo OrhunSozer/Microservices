58b81c35f14d506a90c582805b73d4bf35fd55ec
Orhun Sözer <orhun.sozer@gmail.com>
2022-07-23 13:28:40 +0300

    Created DiscountService in Discount API. Used dapper that it a ORM tool in this service.

--- Services/Discount/FreeCourse.Services.Discount/Services/IDiscountService.cs
+++ Services/Discount/FreeCourse.Services.Discount/Services/IDiscountService.cs
@@ -7,7 +7,7 @@ namespace FreeCourse.Services.Discount.Services
     public interface IDiscountService
     {
         Task<Response<List<Models.Discount>>> GetAll();
-        Task<Response<List<Models.Discount>>> GetById(int id);
+        Task<Response<Models.Discount>> GetById(int id);
         Task<Response<NoContent>> Save(Models.Discount discount);
         Task<Response<NoContent>> Update(Models.Discount discount);
 
