0b2cf0ac6c96db76a91084a8d89d3118baf384ca
Orhun Sözer <orhun.sozer@gmail.com>
2022-07-24 15:05:42 +0300

    Created data transfer objects in Order.Application project.

---
+++ Services/Order/FreeCourse.Services.Order.Application/Dtos/OrderDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Application.Dtos
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public AddressDto Address { get; set; }
+        public string BuyerId { get; set; }
+        public List<OrderItemDto> _orderItems { get; set; }
+    }
+}
