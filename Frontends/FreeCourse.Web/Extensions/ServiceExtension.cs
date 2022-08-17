7bffe92590f98ca2b0c56e204771b4c8db1f991c
Orhun Sözer <orhun.sozer@gmail.com>
2022-08-17 23:42:35 +0300

    I created methods in basket service.

--- Frontends/FreeCourse.Web/Extensions/ServiceExtension.cs
+++ Frontends/FreeCourse.Web/Extensions/ServiceExtension.cs
@@ -41,20 +41,7 @@ namespace FreeCourse.Web.Extensions
                 opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Basket.Path}");
             }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
 
-            services.AddHttpClient<IDiscountService, DiscountService>(opt =>
-            {
-                opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Discount.Path}");
-            }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
-
-            services.AddHttpClient<IPaymentService, PaymentService>(opt =>
-            {
-                opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Payment.Path}");
-            }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
-
-            services.AddHttpClient<IOrderService, OrderService>(opt =>
-            {
-                opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Order.Path}");
-            }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
+           
         }
     }
 }
