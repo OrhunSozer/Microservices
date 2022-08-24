b139caf80790ef09381e45874712939962f94cb4
Orhun Sözer <orhun.sozer@gmail.com>
2022-08-24 22:23:57 +0300

    I created order service.

--- Frontends/FreeCourse.Web/Extensions/ServiceExtension.cs
+++ Frontends/FreeCourse.Web/Extensions/ServiceExtension.cs
@@ -51,6 +51,11 @@ namespace FreeCourse.Web.Extensions
                 opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Payment.Path}");
             }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
 
+            services.AddHttpClient<IOrderService, OrderService>(opt =>
+            {
+                opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Order.Path}");
+            }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
+
         }
     }
 }
---
+++ Frontends/FreeCourse.Web/Services/OrderService.cs
@@ -0,0 +1,25 @@
+﻿using FreeCourse.Web.Models.Orders;
+using FreeCourse.Web.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Services
+{
+    public class OrderService : IOrderService
+    {
+        public Task<OrderCreatedViewModel> CreateOrder(CheckoutInfoInput checkoutInfoInput)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<OrderViewModel>> GetOrder()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<OrderSuspendViewModel> SuspendOrder(CheckoutInfoInput checkoutInfoInput)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
